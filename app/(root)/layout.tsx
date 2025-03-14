c59d7b8957ec901919467bad5527138ba1dd464c
Dercio Anselmo <dercio@eventagrate.com>
2025-03-14 18:02:57 +0400

    Side Bar Menu

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,18 @@
+import Sidebar from "@/components/Sidebar";
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  }: Readonly<{
+    children: React.ReactNode;
+  }>
+) {
+  const loggedIn = {firstName: 'Dercio', lastName: 'Anselmo'};
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar  user={loggedIn}/>
         {children}
     </main>
   );
