a26b7f05bb4966dddec71b180eef881dcfe6ecd5
Dercio Anselmo <dercio@eventagrate.com>
2025-03-17 16:10:42 +0400

    Right Sidebar - Initial BankCard component

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
-export const BankCard = () => {
+export const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
   return (
-    <div>BankCard</div>
+    <div className='flex flex-col'>
+        <Link href="/" className='bank-card'>
+        </Link>
+    </div>
   )
 }
